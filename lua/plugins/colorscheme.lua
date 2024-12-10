2f9efa372a7bc0649e62829db41ddaf5d27aea64
muhrusdi <muhammadrusdiid@gmail.com>
2024-12-10 21:43:24 +0700

    init file

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,55 @@
+return {
+  { "oxfist/night-owl.nvim" },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha", -- latte, frappe, macchiato, mocha
+        term_colors = true,
+        transparent_background = false,
+        no_italic = false,
+        no_bold = false,
+        styles = {
+          comments = {},
+          conditionals = {},
+          loops = {},
+          functions = {},
+          keywords = {},
+          strings = {},
+          variables = {},
+          numbers = {},
+          booleans = {},
+          properties = {},
+          types = {},
+        },
+        color_overrides = {
+          mocha = {
+            base = "#020617",
+            mantle = "#030712",
+            crust = "#000000",
+          },
+        },
+        highlight_overrides = {
+          mocha = function(C)
+            return {
+              TabLineSel = { bg = C.pink },
+              CmpBorder = { fg = C.surface2 },
+              Pmenu = { bg = C.none },
+              IblIndent = { fg = C.surface1 },
+              TelescopeBorder = { link = "FloatBorder" },
+            }
+          end,
+        },
+      })
+
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
